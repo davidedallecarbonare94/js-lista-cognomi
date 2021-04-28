fc0bf93c4a0f609d2a79fdcc365ecad6bf773349
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-04-28 15:41:22 +0200

    minor changes 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,16 @@
-Consegna:
-//1. chiedi all’utente il cognome
-//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-
 var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 
+//1. chiedi all’utente il cognome
 var userSurname = prompt("Inserisci il tuo cognome")
 surnameList.push(userSurname);
 console.log(surnameList)
-
+//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 for (var i = 0; i < surnameList.length; i++) {
     console.log(surnameList[i]);
     //3. stampa la lista ordinata alfabeticamente
     surnameList.sort();
 }
-var position = surnameList.indexOf(userSurname, 0)
+var position = surnameList.indexOf(userSurname, 1)
 console.log(position)
 
 
